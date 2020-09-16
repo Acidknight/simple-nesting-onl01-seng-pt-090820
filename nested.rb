3a6726a7e1a603ca26ab1582d15736469b5e0028
Anthony Leon <acidknights89@gmail.com>
2020-09-16 22:30:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -67,7 +67,7 @@ def adding_matz
 
 	programmer_hash = 
  		{
- 		    :yukohiro_matsumoto => {
+ 		    :yukihiro_matsumoto => {
  		      :known_for => "Ruby",
  		      :languages => ["LISP", "C"]
  		    }
